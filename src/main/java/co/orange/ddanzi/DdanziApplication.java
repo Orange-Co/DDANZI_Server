766bb9a04670b0ad981d2125847a8eb035d33677
kang <kang6@ewhain.net>
2024-04-26 02:01:50 +0900

    chore: initial project setting

---
+++ src/main/java/co/orange/ddanzi/DdanziApplication.java
@@ -0,0 +1,13 @@
+package co.orange.ddanzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdanziApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdanziApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/orange/ddanzi/DdanziApplicationTests.java
@@ -0,0 +1,13 @@
+package co.orange.ddanzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdanziApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

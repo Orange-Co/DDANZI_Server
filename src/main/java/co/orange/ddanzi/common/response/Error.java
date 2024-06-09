f475dd3573299ae6d357a2b64e58fc2773b89f29
kang <kang6@ewhain.net>
2024-06-09 23:22:32 +0900

    feat: add response msg

---
+++ src/main/java/co/orange/ddanzi/common/response/Error.java
@@ -0,0 +1,43 @@
+package co.orange.ddanzi.common.response;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public enum Error {
+    // Default
+    ERROR(HttpStatus.BAD_REQUEST, "Request processing failed"),
+
+    // 400 BAD REQUEST
+
+
+    // 401 UNAUTHORIZED
+
+
+    // 403 Forbidden
+
+
+    // 404 NOT FOUND
+
+
+    // 405 METHOD_NOT_ALLOWED
+
+
+    // 409 CONFLICT
+
+
+
+    // 500 INTERNAL SERVER ERROR
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error"),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    public int getHttpStatusCode() {
+        return httpStatus.value();
+    }
+}

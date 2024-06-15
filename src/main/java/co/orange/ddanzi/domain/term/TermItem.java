a35c76af0252e3ce254debe9b3b31d410d5b01a4
kang <kang6@ewhain.net>
2024-06-16 03:59:10 +0900

    feat: create term entities

---
+++ src/main/java/co/orange/ddanzi/domain/term/TermItem.java
@@ -0,0 +1,30 @@
+package co.orange.ddanzi.domain.term;
+
+import co.orange.ddanzi.common.domain.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class TermItem extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "term_item_id")
+    private Long id;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "is_essential")
+    private Boolean isEssential;
+
+    @Builder
+    public TermItem(Long id, String content, Boolean isEssential) {
+        this.id = id;
+        this.content = content;
+        this.isEssential = isEssential;
+    }
+}
---
+++ src/main/java/co/orange/ddanzi/domain/term/TermOrder.java
@@ -0,0 +1,30 @@
+package co.orange.ddanzi.domain.term;
+
+import co.orange.ddanzi.common.domain.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class TermOrder extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "term_order_id")
+    private Long id;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "is_essential")
+    private Boolean isEssential;
+
+    @Builder
+    public TermOrder(Long id, String content, Boolean isEssential) {
+        this.id = id;
+        this.content = content;
+        this.isEssential = isEssential;
+    }
+}

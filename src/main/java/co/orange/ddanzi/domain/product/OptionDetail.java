d2d4226905101e759871c0db34c245b17c2a49af
kang <kang6@ewhain.net>
2024-06-17 21:46:52 +0900

    feat: create option_detail entity

---
+++ src/main/java/co/orange/ddanzi/domain/product/OptionDetail.java
@@ -0,0 +1,35 @@
+package co.orange.ddanzi.domain.product;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class OptionDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "option_detail_id")
+    private Long id;            //세뷰 옵션 고유 ID
+
+    @Column(name = "content")
+    private String content;     //세부 옵션 내용
+
+    @ColumnDefault("true")
+    @Column(name = "is_available")
+    private Boolean isAvailable;     //선택 가능 여부
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "option_id")
+    private Option option;    //참조하는 옵션
+
+    @Builder
+    public OptionDetail(String content, Boolean isAvailable, Option option) {
+        this.content = content;
+        this.isAvailable = isAvailable;
+        this.option = option;
+    }
+}

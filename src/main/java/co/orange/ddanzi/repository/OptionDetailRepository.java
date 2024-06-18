83f6aa481d19ac56e11712f69bc8063ab79d55a4
kang <kang6@ewhain.net>
2024-06-19 03:13:29 +0900

    fix: typo

--- src/main/java/co/orange/ddanzi/repository/OptionDetailRepository.java
+++ src/main/java/co/orange/ddanzi/repository/OptionDetailRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface OptionDetailRepository extends JpaRepository<OptionDetail, Long> {
-    @Query(value = "SELECT * from option_details od WHERE od.option_i = :optionId", nativeQuery = true)
+    @Query(value = "SELECT * from option_details od WHERE od.option_id = :optionId", nativeQuery = true)
     List<OptionDetail> findAllByOptionId(Long optionId);
 }

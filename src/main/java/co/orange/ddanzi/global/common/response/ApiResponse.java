6af9f5a01c02a5e5be815eac2811a4512672663e
kang <kang6@ewhain.net>
2024-06-18 20:38:15 +0900

    feat: get request path for handler

--- src/main/java/co/orange/ddanzi/global/common/response/ApiResponse.java
+++ src/main/java/co/orange/ddanzi/global/common/response/ApiResponse.java
@@ -1,7 +1,9 @@
 package co.orange.ddanzi.global.common.response;
 
 import co.orange.ddanzi.global.common.exception.Error;
+import co.orange.ddanzi.global.config.handler.GlobalControllerHandler;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.Builder;
 import lombok.ToString;
@@ -11,19 +13,22 @@ import java.time.LocalDateTime;
 @ToString
 @Builder
 public class ApiResponse<T> {
-    @JsonFormat(
-            shape = JsonFormat.Shape.STRING,
-            pattern = "yyyy-MM-dd HH:mm:ss",
-            locale = "Asia/Seoul"
-    )
 
+    @JsonProperty
     private int status;
+    @JsonProperty
     private String message;
+    @JsonProperty
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime timestamp;
+    @JsonProperty
     private String path;
+    @JsonProperty
     private final T data;
 
-    public static <T> ApiResponse<T> onSuccess(Success success, HttpServletRequest request, T data) {
+
+    public static <T> ApiResponse<T> onSuccess(Success success, T data) {
+        HttpServletRequest request = GlobalControllerHandler.getRequest();
         return new ApiResponse<>(
                 success.getHttpStatusCode(),
                 success.getMessage(),
@@ -33,7 +38,8 @@ public class ApiResponse<T> {
         );
     }
 
-    public static <T> ApiResponse<T> onFailure(Error error, HttpServletRequest request, T data) {
+    public static <T> ApiResponse<T> onFailure(Error error, T data) {
+        HttpServletRequest request = GlobalControllerHandler.getRequest();
         return new ApiResponse<>(
                 error.getHttpStatusCode(),
                 error.getMessage(),

16673868e20ca68ab63a361ea2e4b32a25009214
kang <kang6@ewhain.net>
2024-06-18 21:02:43 +0900

    feat: create get home detail response dto

---
+++ src/main/java/co/orange/ddanzi/dto/home/OptionDetailInfo.java
@@ -0,0 +1,11 @@
+package co.orange.ddanzi.dto.home;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class OptionDetailInfo {
+    private Long optionDetailId;
+    private String content;
+}

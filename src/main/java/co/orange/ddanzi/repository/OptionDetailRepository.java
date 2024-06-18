9e56ec677206d2e3b03317fe2e7d21992a9fda3c
kang <kang6@ewhain.net>
2024-06-19 01:32:46 +0900

    feat: create home detail api

--- src/main/java/co/orange/ddanzi/repository/OptionDetailRepository.java
+++ src/main/java/co/orange/ddanzi/repository/OptionDetailRepository.java
@@ -2,6 +2,11 @@ package co.orange.ddanzi.repository;
 
 import co.orange.ddanzi.domain.product.OptionDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface OptionDetailRepository extends JpaRepository<OptionDetail, Long> {
+    @Query(value = "SELECT * from option_details od WHERE od.option_i = :optionId", nativeQuery = true)
+    List<OptionDetail> findAllByOptionId(Long optionId);
 }
--- src/main/java/co/orange/ddanzi/repository/OptionRepository.java
+++ src/main/java/co/orange/ddanzi/repository/OptionRepository.java
@@ -2,6 +2,11 @@ package co.orange.ddanzi.repository;
 
 import co.orange.ddanzi.domain.product.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface OptionRepository extends JpaRepository<Option, Long> {
+    @Query(value = "SELECT * FROM options o WHERE o.product_id = :productId", nativeQuery = true)
+    List<Option> findAllByProductId(Long productId);
 }

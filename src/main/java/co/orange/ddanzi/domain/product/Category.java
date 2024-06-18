42201bcdc5c782c2dcbf59e65eee2fe79fc417cb
kang <kang6@ewhain.net>
2024-06-19 00:13:03 +0900

    feat: separate discount field to entity

--- src/main/java/co/orange/ddanzi/domain/product/Category.java
+++ src/main/java/co/orange/ddanzi/domain/product/Category.java
@@ -27,23 +27,22 @@ public class Category extends BaseTimeEntity {
     @Column(name = "is_forbidden", nullable = false)
     private Boolean isForbidden;                //금지 여부
 
-    @ColumnDefault("0.3")
-    @Column(name = "discount_rate")
-    private Float discountRate;                 //할인율
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "discount_id", nullable = true)
+    private Discount discount;                  //할인율
 
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "parentCategory", cascade = CascadeType.ALL)
-    private List<Category> childrenCategory = new ArrayList<>();
+    private List<Category> childrenCategory = new ArrayList<>();    //자식 카테고리
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_category_id", nullable = true)
-    private Category parentCategory;
+    private Category parentCategory;                                //부모 카테고리
 
     @Builder
-    public Category(Long id, String content, Boolean isForbidden, Float discountRate) {
+    public Category(Long id, String content, Boolean isForbidden) {
         this.id = id;
         this.content = content;
         this.isForbidden = isForbidden;
-        this.discountRate = discountRate;
     }
 
     public String getFullPath(){

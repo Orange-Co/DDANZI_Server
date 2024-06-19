1dac11009ec0f0612ac94ca4a5b164cf9fc97c21
kang <kang6@ewhain.net>
2024-06-19 23:50:21 +0900

    feat:add query to find address for user

--- src/main/java/co/orange/ddanzi/repository/AddressRepository.java
+++ src/main/java/co/orange/ddanzi/repository/AddressRepository.java
@@ -1,8 +1,11 @@
 package co.orange.ddanzi.repository;
 
 import co.orange.ddanzi.domain.user.Address;
+import co.orange.ddanzi.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
-
+    @Query("SELECT a FROM Address a WHERE a.user = :user")
+    Address findByUser(User user);
 }

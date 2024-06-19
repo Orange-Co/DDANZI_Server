3d124a8a9511094e72bf97823197f9d47c440253
kang <kang6@ewhain.net>
2024-06-19 17:13:41 +0900

    feat: get interestCount data to count for interestProduct entity

--- src/main/java/co/orange/ddanzi/repository/InterestProductRepository.java
+++ src/main/java/co/orange/ddanzi/repository/InterestProductRepository.java
@@ -10,6 +10,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface InterestProductRepository extends JpaRepository<InterestProduct, Long> {
+
+    @Query("SELECT CASE WHEN COUNT(ip) > 1000 THEN 999 ELSE COUNT(ip) END FROM InterestProduct ip " +
+            "WHERE ip.id.product.id = :productId")
+    Integer countByProductIdWithLimit(@Param("productId") Long productId);
+
     @Modifying
     @Transactional
     @Query("DELETE FROM InterestProduct ip WHERE ip.id.user = :user AND ip.id.product = :product")

31b954eaad9ce061ecd447532f930c5dfeb16a27
kang <kang6@ewhain.net>
2024-06-20 02:16:55 +0900

    feat: create push alarm repository

---
+++ src/main/java/co/orange/ddanzi/repository/PushAlarmRepository.java
@@ -0,0 +1,12 @@
+package co.orange.ddanzi.repository;
+
+import co.orange.ddanzi.domain.user.PushAlarm;
+import co.orange.ddanzi.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface PushAlarmRepository extends JpaRepository<PushAlarm, Long> {
+    @Query("SELECT pa FROM PushAlarm pa WHERE pa.user = :user ")
+    PushAlarm findByUser(@Param("user") User user);
+}

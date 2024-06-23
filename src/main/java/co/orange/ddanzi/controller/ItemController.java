cbb179a5901073c5d63e5a67f975dc0c663bf125
kang <kang6@ewhain.net>
2024-06-23 16:04:52 +0900

    feat: create basic structure of item apis

---
+++ src/main/java/co/orange/ddanzi/controller/ItemController.java
@@ -0,0 +1,11 @@
+package co.orange.ddanzi.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/item")
+@RestController
+public class ItemController {
+}

1130cc50419fb2a7d88f11afb76b4adc9108efc6
kang <kang6@ewhain.net>
2024-06-24 09:16:09 +0900

    feat: add query

--- src/main/java/co/orange/ddanzi/repository/ProductRepository.java
+++ src/main/java/co/orange/ddanzi/repository/ProductRepository.java
@@ -8,6 +8,10 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("SELECT p FROM Product p WHERE p.kakaoProductId = :kakaoProductId")
+    Product findByKakaoProductId(Long kakaoProductId);
+
     @Query(value = "SELECT * FROM products p WHERE p.stock > :stock", nativeQuery = true)
     List<Product> findAllByStock(Integer stock);
 

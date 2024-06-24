917b2525617021e4b2c8ff64e02b15551ff992f9
kang <kang6@ewhain.net>
2024-06-24 09:16:02 +0900

    feat: create confirm product api

--- src/main/java/co/orange/ddanzi/controller/ItemController.java
+++ src/main/java/co/orange/ddanzi/controller/ItemController.java
@@ -1,6 +1,11 @@
 package co.orange.ddanzi.controller;
 
+import co.orange.ddanzi.dto.item.ConfirmProductRequestDto;
+import co.orange.ddanzi.global.common.response.ApiResponse;
+import co.orange.ddanzi.service.ItemService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/item")
 @RestController
 public class ItemController {
+    private final ItemService itemService;
+    @GetMapping("/confirm")
+    ApiResponse<?> confirmProduct(@RequestBody ConfirmProductRequestDto requestDto){
+        return itemService.confirmProduct(requestDto);
+    }
 }
--- src/main/java/co/orange/ddanzi/service/ItemService.java
+++ src/main/java/co/orange/ddanzi/service/ItemService.java
@@ -1,5 +1,12 @@
 package co.orange.ddanzi.service;
 
+import co.orange.ddanzi.domain.product.Product;
+import co.orange.ddanzi.dto.item.ConfirmProductRequestDto;
+import co.orange.ddanzi.dto.item.ConfirmProductResponseDto;
+import co.orange.ddanzi.global.common.response.ApiResponse;
+import co.orange.ddanzi.global.common.response.Success;
+import co.orange.ddanzi.repository.ItemRepository;
+import co.orange.ddanzi.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -8,4 +15,19 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class ItemService {
+    private final ProductRepository productRepository;
+    private final ItemRepository itemRepository;
+
+    public ApiResponse<?> confirmProduct(ConfirmProductRequestDto requestDto){
+        Product product = productRepository.findByKakaoProductId(requestDto.getKakaoProductId());
+        if(product == null){
+        }
+        ConfirmProductResponseDto responseDto = ConfirmProductResponseDto.builder()
+                .productId(product.getId())
+                .productName(product.getName())
+                .originPrice(product.getOriginPrice())
+                .salePrice(product.getOriginPrice() - product.getDiscountPrice())
+                .build();
+        return ApiResponse.onSuccess(Success.CREATE_PRODUCT_SUCCESS, responseDto);
+    }
 }

6169086bfd907faf4ec91d4fb47257b180a623b5
kang <kang6@ewhain.net>
2024-06-25 19:32:23 +0900

    feat: change http method

--- src/main/java/co/orange/ddanzi/controller/ItemController.java
+++ src/main/java/co/orange/ddanzi/controller/ItemController.java
@@ -4,17 +4,14 @@ import co.orange.ddanzi.dto.item.ConfirmProductRequestDto;
 import co.orange.ddanzi.global.common.response.ApiResponse;
 import co.orange.ddanzi.service.ItemService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/item")
 @RestController
 public class ItemController {
     private final ItemService itemService;
-    @GetMapping("/confirm")
+    @PostMapping("/confirm")
     ApiResponse<?> confirmProduct(@RequestBody ConfirmProductRequestDto requestDto){
         return itemService.confirmProduct(requestDto);
     }

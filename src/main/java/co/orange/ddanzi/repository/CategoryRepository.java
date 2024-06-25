555515fc5138238fc3521854db6a778dc0a39f5e
kang <kang6@ewhain.net>
2024-06-25 21:37:48 +0900

    feat: create findByContent Query

--- src/main/java/co/orange/ddanzi/repository/CategoryRepository.java
+++ src/main/java/co/orange/ddanzi/repository/CategoryRepository.java
@@ -3,11 +3,15 @@ package co.orange.ddanzi.repository;
 import co.orange.ddanzi.domain.product.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     @Query("SELECT c FROM Category c WHERE c.content = :content AND c.parentCategory = :parentCategory")
-    Optional<Category> findByContentAndParentCategory(String content, Category parentCategory);
+    Optional<Category> findByContentAndParentCategory(@Param("content") String content, @Param("parentCategory") Category parentCategory);
+
+    @Query("SELECT c FROM Category c WHERE c.content = :content")
+    Optional<Category> findByContent(String content);
 
 }

82be467550acb99bc3eb864974ba18d621d3051d
kang <kang6@ewhain.net>
2024-06-26 00:47:04 +0900

    fix: modify pk data type

--- src/main/java/co/orange/ddanzi/repository/ItemRepository.java
+++ src/main/java/co/orange/ddanzi/repository/ItemRepository.java
@@ -3,5 +3,7 @@ package co.orange.ddanzi.repository;
 import co.orange.ddanzi.domain.product.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ItemRepository extends JpaRepository<Item, Long> {
+import java.util.UUID;
+
+public interface ItemRepository extends JpaRepository<Item, UUID> {
 }

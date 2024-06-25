2f1b05d455b4e561134e0ec565be214985f2261d
kang <kang6@ewhain.net>
2024-06-25 14:25:51 +0900

    feat: add request objects and toEntity method

--- src/main/java/co/orange/ddanzi/dto/item/ConfirmProductRequestDto.java
+++ src/main/java/co/orange/ddanzi/dto/item/ConfirmProductRequestDto.java
@@ -1,10 +1,35 @@
 package co.orange.ddanzi.dto.item;
 
+import co.orange.ddanzi.domain.product.Category;
+import co.orange.ddanzi.domain.product.Product;
 import lombok.Getter;
 
 @Getter
 public class ConfirmProductRequestDto {
     private Long kakaoProductId;
     private String productName;
+    private Integer originPrice;
+    private String imgUrl;
+    private String infoUrl;
     private String category;
+    private Boolean isForbidden;
+
+    public Category toCategory(String content){
+        return Category.builder()
+                .content(content)
+                .isForbidden(isForbidden)
+                .build();
+    }
+
+    public Product toProduct(Integer discountPrice){
+        return Product.builder()
+                .kakaoProductId(kakaoProductId)
+                .name(productName)
+                .originPrice(originPrice)
+                .discountPrice(discountPrice)
+                .imgUrl(imgUrl)
+                .infoUrl(infoUrl)
+                .stock(0)
+                .build();
+    }
 }

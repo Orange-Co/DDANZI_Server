0a7a297a436c923e7e919812aeb9704f75218f93
kang <kang6@ewhain.net>
2024-06-25 21:37:27 +0900

    chore: add comment

--- src/main/java/co/orange/ddanzi/service/ItemService.java
+++ src/main/java/co/orange/ddanzi/service/ItemService.java
@@ -6,9 +6,9 @@ import co.orange.ddanzi.dto.item.ConfirmProductRequestDto;
 import co.orange.ddanzi.dto.item.ConfirmProductResponseDto;
 import co.orange.ddanzi.global.common.response.ApiResponse;
 import co.orange.ddanzi.global.common.response.Success;
-import co.orange.ddanzi.repository.CategoryRepository;
 import co.orange.ddanzi.repository.ItemRepository;
 import co.orange.ddanzi.repository.ProductRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.util.Pair;
@@ -22,18 +22,19 @@ public class ItemService {
     private final ItemRepository itemRepository;
     private final CategoryService categoryService;
 
+    @Transactional
     public ApiResponse<?> confirmProduct(ConfirmProductRequestDto requestDto){
         Product product = productRepository.findByKakaoProductId(requestDto.getKakaoProductId());
         if(product == null){
             Pair<Category, Float> leafCategoryAndDiscountRate = categoryService.createOrGetCategory(requestDto.getCategory(), requestDto.getIsForbidden());
-
+            log.info("leaf category 찾기 성공 category_id -> {}",leafCategoryAndDiscountRate.getFirst().getId());
             Integer discountPrice = (int)(requestDto.getOriginPrice()* leafCategoryAndDiscountRate.getSecond());
             log.info("할인가 계산 완료 -> {}",discountPrice);
 
             Product newProduct = requestDto.toProduct(discountPrice, leafCategoryAndDiscountRate.getFirst());
             productRepository.save(newProduct);
-            log.info("상품 등록 완료 -> product_id: {}",product.getId());
             product = newProduct;
+            log.info("상품 등록 완료 -> product_id: {}",product.getId());
         }
         ConfirmProductResponseDto responseDto = ConfirmProductResponseDto.builder()
                 .productId(product.getId())

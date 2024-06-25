2265eb2f0c30aecf8743fe0e9fa6360b664436b5
kang <kang6@ewhain.net>
2024-06-25 20:12:31 +0900

    feat: create category if it doesn't exist

--- src/main/java/co/orange/ddanzi/repository/CategoryRepository.java
+++ src/main/java/co/orange/ddanzi/repository/CategoryRepository.java
@@ -2,7 +2,12 @@ package co.orange.ddanzi.repository;
 
 import co.orange.ddanzi.domain.product.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    @Query("SELECT c FROM Category c WHERE c.content = :content AND c.parentCategory = :parentCategory")
+    Optional<Category> findByContentAndParentCategory(String content, Category parentCategory);
 
 }
--- src/main/java/co/orange/ddanzi/service/ItemService.java
+++ src/main/java/co/orange/ddanzi/service/ItemService.java
@@ -1,14 +1,17 @@
 package co.orange.ddanzi.service;
 
+import co.orange.ddanzi.domain.product.Category;
 import co.orange.ddanzi.domain.product.Product;
 import co.orange.ddanzi.dto.item.ConfirmProductRequestDto;
 import co.orange.ddanzi.dto.item.ConfirmProductResponseDto;
 import co.orange.ddanzi.global.common.response.ApiResponse;
 import co.orange.ddanzi.global.common.response.Success;
+import co.orange.ddanzi.repository.CategoryRepository;
 import co.orange.ddanzi.repository.ItemRepository;
 import co.orange.ddanzi.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -17,10 +20,20 @@ import org.springframework.stereotype.Service;
 public class ItemService {
     private final ProductRepository productRepository;
     private final ItemRepository itemRepository;
+    private final CategoryService categoryService;
 
     public ApiResponse<?> confirmProduct(ConfirmProductRequestDto requestDto){
         Product product = productRepository.findByKakaoProductId(requestDto.getKakaoProductId());
         if(product == null){
+            Pair<Category, Float> leafCategoryAndDiscountRate = categoryService.createOrGetCategory(requestDto.getCategory(), requestDto.getIsForbidden());
+
+            Integer discountPrice = (int)(requestDto.getOriginPrice()* leafCategoryAndDiscountRate.getSecond());
+            log.info("할인가 계산 완료 -> {}",discountPrice);
+
+            Product newProduct = requestDto.toProduct(discountPrice, leafCategoryAndDiscountRate.getFirst());
+            productRepository.save(newProduct);
+            log.info("상품 등록 완료 -> product_id: {}",product.getId());
+            product = newProduct;
         }
         ConfirmProductResponseDto responseDto = ConfirmProductResponseDto.builder()
                 .productId(product.getId())

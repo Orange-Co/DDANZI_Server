257166765a3468b71edefb4af1e8dbd7063eb3bc
kang <kang6@ewhain.net>
2024-06-27 04:50:23 +0900

    feat: create item's PK when save new item

--- src/main/java/co/orange/ddanzi/dto/item/SaveItemRequestDto.java
+++ src/main/java/co/orange/ddanzi/dto/item/SaveItemRequestDto.java
@@ -18,8 +18,9 @@ public class SaveItemRequestDto {
     private LocalDate dueDate;
     private List<TermInfo> termItem;
 
-    public Item toItem(User user, Product product){
+    public Item toItem(String id, User user, Product product){
         return Item.builder()
+                .id(id)
                 .product(product)
                 .seller(user)
                 .imgUrl(itemImgUrl)
--- src/main/java/co/orange/ddanzi/service/ItemService.java
+++ src/main/java/co/orange/ddanzi/service/ItemService.java
@@ -1,11 +1,8 @@
 package co.orange.ddanzi.service;
 
-import co.orange.ddanzi.domain.product.Category;
 import co.orange.ddanzi.domain.product.Item;
 import co.orange.ddanzi.domain.product.Product;
 import co.orange.ddanzi.domain.user.User;
-import co.orange.ddanzi.dto.item.ConfirmProductRequestDto;
-import co.orange.ddanzi.dto.item.ConfirmProductResponseDto;
 import co.orange.ddanzi.dto.item.SaveItemRequestDto;
 import co.orange.ddanzi.dto.item.SaveItemResponseDto;
 import co.orange.ddanzi.global.common.exception.Error;
@@ -15,8 +12,10 @@ import co.orange.ddanzi.repository.*;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 
 @Slf4j
@@ -32,7 +31,8 @@ public class ItemService {
         if(product == null)
             return ApiResponse.onFailure(Error.PRODUCT_NOT_FOUND, null);
 
-        Item newItem = requestDto.toItem(user, product);
+        String itemId = createItemId(product);
+        Item newItem = requestDto.toItem(itemId, user, product);
         newItem = itemRepository.save(newItem);
         log.info("item 등록 성공, item_id: {}",newItem.getId());
 
@@ -45,5 +45,26 @@ public class ItemService {
                 .originPrice(product.getOriginPrice())
                 .build();
         return ApiResponse.onSuccess(Success.CREATE_ITEM_SUCCESS, responseDto);
+    }
+
+
+    public String createItemId(Product product) {
+        String productId = product.getId();
+
+        log.info("업로드 일자 포멧팅");
+        LocalDate uploadDate= LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyMMdd");
+        String formattedDate = uploadDate.format(formatter);
+
+        log.info("{}의 max sequenceNumber 찾기",product.getName());
+        Integer maxSequenceNumber = itemRepository.findMaxSequenceNumberByProduct(product);
+        int nextSequenceNumber = (maxSequenceNumber != null ? maxSequenceNumber + 1 : 1);
+        String sequenceNumberStr = String.format("%02d", nextSequenceNumber);
+        log.info("{}의 next sequenceNumber -> {}",product.getName(),nextSequenceNumber);
+
+        String itemId = productId + formattedDate + sequenceNumberStr;
+        log.info("item_id 생성 완료 -> {}",itemId);
+
+        return itemId;
     }
 }

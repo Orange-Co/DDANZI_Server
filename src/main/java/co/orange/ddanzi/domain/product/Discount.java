725d310c5e134b31ce16f6d44245b17f82b764f2
kang <kang6@ewhain.net>
2024-07-17 01:27:29 +0900

    feat: create discount entity

---
+++ src/main/java/co/orange/ddanzi/domain/product/Discount.java
@@ -0,0 +1,37 @@
+package co.orange.ddanzi.domain.product;
+
+import co.orange.ddanzi.domain.user.User;
+import co.orange.ddanzi.global.common.domain.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+@Getter
+@NoArgsConstructor
+@Table(name = "discounts")
+@Entity
+public class Discount extends BaseTimeEntity {
+    @Id
+    @Column(name = "product_id")
+    private Long id;      //상품 고유 ID (PK/FK)
+
+    @MapsId
+    @OneToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Column(name = "discount_rate", nullable = false)
+    private Float discountRate;
+
+    @Column(name = "discount_price", nullable = false)
+    private Integer discountPrice;
+
+    @Builder
+    public Discount(Product product, Float discountRate, Integer discountPrice) {
+        this.product = product;
+        this.discountRate = discountRate;
+        this.discountPrice = discountPrice;
+    }
+}

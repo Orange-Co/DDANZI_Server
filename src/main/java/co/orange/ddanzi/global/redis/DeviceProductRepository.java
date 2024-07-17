022ec7cc7270da18aa6ff466eedeaaa28f5d8173
kang <kang6@ewhain.net>
2024-07-18 02:01:25 +0900

    feat: delete unused file

--- src/main/java/co/orange/ddanzi/global/redis/DeviceProductRepository.java
+++
@@ -1,9 +0,0 @@
-package co.orange.ddanzi.global.redis;
-
-import java.util.Set;
-
-public interface DeviceProductRepository {
-    void addProductToDevice(String deviceToken, String productId);
-    Set<String> getProductIdsForDevice(String deviceToken);
-    boolean isProductAssociatedWithDevice(String deviceToken, String productId);
-}

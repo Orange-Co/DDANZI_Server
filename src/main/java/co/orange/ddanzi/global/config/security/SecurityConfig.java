4b01c6a1be4dbd8b23e6d202e8d650723ec86656
kang <kang6@ewhain.net>
2024-08-08 01:01:30 +0900

    chore: add debug option

--- src/main/java/co/orange/ddanzi/global/config/security/SecurityConfig.java
+++ src/main/java/co/orange/ddanzi/global/config/security/SecurityConfig.java
@@ -3,40 +3,40 @@ package co.orange.ddanzi.global.config.security;
 import co.orange.ddanzi.global.config.jwt.JwtFilter;
 import co.orange.ddanzi.global.config.jwt.JwtUtils;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Configurable
-@EnableWebSecurity
+@Configuration
 @RequiredArgsConstructor
+@EnableWebSecurity(debug = true)
 public class SecurityConfig {
 
     private final JwtUtils jwtUtils;
 
     private String[] permitList = {
             "/api/v1/auth/**",
+            "/api/v1/auth/signin/test",
             "/api/v1/home/**",
-            "/api/v1/search/**",
+            "/api/v1/search/**"
     };
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .httpBasic(HttpBasicConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .sessionManagement(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests((authorizeRequests) -> {
-                    authorizeRequests
-                            .requestMatchers(permitList).permitAll()
-                            .anyRequest().authenticated();
-                })
+                .authorizeHttpRequests((authorizeRequests) ->
+                        authorizeRequests
+                        .requestMatchers(permitList).permitAll()
+                        .anyRequest().authenticated()
+                )
                 .addFilterBefore(new JwtFilter(jwtUtils), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }

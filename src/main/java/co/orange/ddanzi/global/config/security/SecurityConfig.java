7449fbcd170d1361e07df7ef2e84ece9f448b29a
kang <kang6@ewhain.net>
2024-08-11 14:46:06 +0900

    feat: update security setting

--- src/main/java/co/orange/ddanzi/global/config/security/SecurityConfig.java
+++ src/main/java/co/orange/ddanzi/global/config/security/SecurityConfig.java
@@ -19,8 +19,7 @@ public class SecurityConfig {
     private final JwtUtils jwtUtils;
 
     private String[] permitList = {
-            "/api/v1/auth/**",
-            "/api/v1/auth/signin/test",
+            "/api/v1/auth/signin",
             "/api/v1/home/**",
             "/api/v1/search/**"
     };

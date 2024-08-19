02504df7b95d93a815f1fbc0008309b5fb2bb61c
kang <kang6@ewhain.net>
2024-08-20 06:07:55 +0900

    chore: get request from RequestContextHolder

--- src/main/java/co/orange/ddanzi/global/handler/GlobalControllerHandler.java
+++ src/main/java/co/orange/ddanzi/global/handler/GlobalControllerHandler.java
@@ -3,17 +3,19 @@ package co.orange.ddanzi.global.handler;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @ControllerAdvice
 public class GlobalControllerHandler {
-    private static final ThreadLocal<HttpServletRequest> requestHolder = new ThreadLocal<>();
 
     @ModelAttribute
     public void setRequest(HttpServletRequest request) {
-        requestHolder.set(request);
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
     }
 
     public static HttpServletRequest getRequest() {
-        return requestHolder.get();
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        return attributes != null ? attributes.getRequest() : null;
     }
 }

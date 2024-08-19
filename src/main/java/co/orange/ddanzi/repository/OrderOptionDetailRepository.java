32d01e08b6997025d7a76613379d6f1338594feb
kang <kang6@ewhain.net>
2024-08-20 03:43:25 +0900

    feat: create order option detail entity

---
+++ src/main/java/co/orange/ddanzi/repository/OrderOptionDetailRepository.java
@@ -0,0 +1,7 @@
+package co.orange.ddanzi.repository;
+
+import co.orange.ddanzi.domain.order.OrderOptionDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderOptionDetailRepository extends JpaRepository<OrderOptionDetail, Long> {
+}

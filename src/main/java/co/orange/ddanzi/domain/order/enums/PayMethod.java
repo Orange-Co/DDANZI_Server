3f4ccaa27b4d7af58acd2243c87ee14d469fa3a2
kang <kang6@ewhain.net>
2024-08-20 00:48:00 +0900

    chore: update payment method

--- src/main/java/co/orange/ddanzi/domain/order/enums/PayMethod.java
+++ src/main/java/co/orange/ddanzi/domain/order/enums/PayMethod.java
@@ -1,8 +1,12 @@
 package co.orange.ddanzi.domain.order.enums;
 
 public enum PayMethod {
-    CARD("카드"),
-    NAVERPAY("네이버페이")
+    CARD("신용/체크카드"),
+    NAVERPAY("네이버페이"),
+    KAKAOPAY("카카오페이"),
+    SAMSUNGPAY("삼성페이"),
+    ACCOUNT("계좌이체"),
+    PHONE("휴대폰 결제")
     ;
 
     private String payMethod;

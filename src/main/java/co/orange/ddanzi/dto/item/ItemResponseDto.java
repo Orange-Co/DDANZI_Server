b4161a74ba8092999b6e0efc67a6c470c6c7e45b
kang <kang6@ewhain.net>
2024-08-31 15:20:04 +0900

    feat: cerate res dto for get item api

---
+++ src/main/java/co/orange/ddanzi/dto/item/ItemResponseDto.java
@@ -0,0 +1,21 @@
+package co.orange.ddanzi.dto.item;
+
+
+import co.orange.ddanzi.dto.AddressInfo;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ItemResponseDto {
+    private String itemId;
+    private String status;
+    private String productName;
+    private Integer originPrice;
+    private Integer salePrice;
+    private String orderId;
+    private String buyerNickName;
+    private AddressInfo addressInfo;
+    private String paidAt;
+    private String paymentMethod;
+}

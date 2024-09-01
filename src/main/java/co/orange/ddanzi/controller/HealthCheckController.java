6ea179a5e76b1507ea0d0477061cd7674b0d7c6c
kang <kang6@ewhain.net>
2024-09-01 22:15:50 +0900

    feat: add health check api

---
+++ src/main/java/co/orange/ddanzi/controller/HealthCheckController.java
@@ -0,0 +1,19 @@
+package co.orange.ddanzi.controller;
+
+import co.orange.ddanzi.common.response.ApiResponse;
+import co.orange.ddanzi.common.response.Success;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+public class HealthCheckController {
+
+    @GetMapping("/heath-check")
+    @ResponseStatus(HttpStatus.OK)
+    public ApiResponse<?> healthCheck() {
+        return ApiResponse.onSuccess(Success.SUCCESS,null);
+    }
+
+}

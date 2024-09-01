d0a5606c5fc40098fbb355d2936dba4da9dd81f0
kang <kang6@ewhain.net>
2024-09-01 13:16:03 +0900

    feat: add query

--- src/main/java/co/orange/ddanzi/repository/ItemRepository.java
+++ src/main/java/co/orange/ddanzi/repository/ItemRepository.java
@@ -2,6 +2,7 @@ package co.orange.ddanzi.repository;
 
 import co.orange.ddanzi.domain.product.Item;
 import co.orange.ddanzi.domain.product.Product;
+import co.orange.ddanzi.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,6 +12,8 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ItemRepository extends JpaRepository<Item, String> {
+    List<Item> findAllBySeller(User user);
+
     @Query("SELECT MAX(CAST(SUBSTRING(i.id, 14, 2) AS integer)) FROM Item i WHERE i.product = :product")
     Integer findMaxSequenceNumberByProduct(@Param("product") Product product);
 
@@ -24,5 +27,4 @@ public interface ItemRepository extends JpaRepository<Item, String> {
 
     @Query("SELECT i FROM Item  i WHERE i.dueDate < :currentDate")
     List<Item> findExpiryItems(@Param("currentDate") LocalDate currentDate);
-
 }

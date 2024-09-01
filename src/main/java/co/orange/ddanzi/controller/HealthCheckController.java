00a116eafe7a31a145e7243dc0f6fa9f52e560ce
kang <kang6@ewhain.net>
2024-09-01 22:37:58 +0900

    fix: change return type

--- src/main/java/co/orange/ddanzi/controller/HealthCheckController.java
+++ src/main/java/co/orange/ddanzi/controller/HealthCheckController.java
@@ -1,19 +1,15 @@
 package co.orange.ddanzi.controller;
 
-import co.orange.ddanzi.common.response.ApiResponse;
-import co.orange.ddanzi.common.response.Success;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class HealthCheckController {
 
     @GetMapping("/health-check")
-    @ResponseStatus(HttpStatus.OK)
-    public ApiResponse<?> healthCheck() {
-        return ApiResponse.onSuccess(Success.SUCCESS,null);
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("OK");
     }
 
 }

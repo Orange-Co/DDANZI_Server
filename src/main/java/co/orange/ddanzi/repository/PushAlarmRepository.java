431d05d849b7b40e242f3da8eba2333c1d438e32
kang <kang6@ewhain.net>
2024-09-08 18:04:11 +0900

    feat: check push alarm exists

--- src/main/java/co/orange/ddanzi/repository/PushAlarmRepository.java
+++ src/main/java/co/orange/ddanzi/repository/PushAlarmRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface PushAlarmRepository extends JpaRepository<PushAlarm, Long> {
     @Query("SELECT pa FROM PushAlarm pa WHERE pa.user = :user ")
-    PushAlarm findByUser(@Param("user") User user);
+    Optional<PushAlarm> findByUser(@Param("user") User user);
 }

ca559d3a79908db8ec59933ae5951a224d69273f
kang <kang6@ewhain.net>
2024-09-09 15:38:01 +0900

    feat: add annotation

--- src/main/java/co/orange/ddanzi/common/domain/BaseTimeEntity.java
+++ src/main/java/co/orange/ddanzi/common/domain/BaseTimeEntity.java
@@ -1,10 +1,15 @@
 package co.orange.ddanzi.common.domain;
 
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
+
+@Getter
+@MappedSuperclass
 public class BaseTimeEntity {
     @CreatedDate
     LocalDateTime createdAt;

8a3ab220f044453166ed7576eb93eb6fd24202d1
kang <kang6@ewhain.net>
2024-09-12 03:31:10 +0900

    fix: null exception

--- src/main/java/co/orange/ddanzi/service/ItemService.java
+++ src/main/java/co/orange/ddanzi/service/ItemService.java
@@ -119,7 +119,7 @@ public class ItemService {
                 .salePrice(product.getOriginPrice()-discount.getDiscountPrice())
                 .orderId(order != null ? order.getId() : null)
                 .buyerNickName(order!=null ? order.getBuyer().getNickname() : null)
-                .addressInfo(order!=null ? addressService.setAddressInfo(order.getBuyer()) : null)
+                .addressInfo(order!=null ? addressService.setAddressInfo(order.getBuyer()) : addressService.setAddressInfo(null))
                 .paidAt(payment!=null ? payment.getEndedAt():null)
                 .paymentMethod(payment!=null ? payment.getMethod():null)
                 .build();

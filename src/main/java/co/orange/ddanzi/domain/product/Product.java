dd804c7e321ff29ecd3a2ee5b268da4cf7cad5cd
kang <kang6@ewhain.net>
2024-09-19 00:55:46 +0900

    feat: add closestDueDate column

--- src/main/java/co/orange/ddanzi/domain/product/Product.java
+++ src/main/java/co/orange/ddanzi/domain/product/Product.java
@@ -1,10 +1,11 @@
 package co.orange.ddanzi.domain.product;
 
 import jakarta.persistence.*;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDate;
 
 @Getter
 @NoArgsConstructor
@@ -40,22 +41,12 @@ public class Product {
     @Column(name = "info_url")
     private String infoUrl;             //상품 상세 정보 url
 
+    @Column(name = "closest_due_date")
+    private LocalDate closestDueDate;   //가장 가까운 만료일
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "leaf_category_id")
     private Category leafCategory;      //리프 카테고리
-
-    @Builder
-    public Product (String id, Long kakaoProductId, String name, String originName, Integer originPrice, String imgUrl, String infoUrl, Integer stock, Category leafCategory) {
-        this.id = id;
-        this.kakaoProductId = kakaoProductId;
-        this.name = name;
-        this.originName = originName;
-        this.originPrice = originPrice;
-        this.imgUrl = imgUrl;
-        this.infoUrl = infoUrl;
-        this.stock = stock;
-        this.leafCategory = leafCategory;
-    }
 
     public void updateStock(Integer stock) {
         this.stock = stock;

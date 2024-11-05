7676fbfae80dbe6c5305e9a71494a9c8e359b4c3
kang <kang6@ewhain.net>
2024-11-05 16:45:41 +0900

    chore: add json property

--- src/main/java/co/orange/ddanzi/dto/payment/PortOneTokenRequestDto.java
+++ src/main/java/co/orange/ddanzi/dto/payment/PortOneTokenRequestDto.java
@@ -1,5 +1,6 @@
 package co.orange.ddanzi.dto.payment;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,6 +11,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PortOneTokenRequestDto {
+    @JsonProperty("imp_key")
     private String imp_key;
+    @JsonProperty("imp_secret")
     private String imp_secret;
 }
